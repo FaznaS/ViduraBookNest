9366b327482c535c5b17b1603c4c2c6576ff7944
DESKTOP-L8A9RC2\Fazna <gfsheriffdeen@gmail.com>
2024-12-12 20:14:23 +0530

    updated to add images of book cover

--- manageBookRecords.php
+++ manageBookRecords.php
@@ -1,18 +1,20 @@
 <?php 
     // Setting up the connection
-    $conn = mysqli_connect("localhost", "root", "fazna11aa99zz@Sheriffdeen", "booknest_db");
+    $conn = mysqli_connect("localhost", "root", "", "booknest_db");
 
     if (!$conn) {
         die("Connection Unsuccessful: " . mysqli_connect_error());
     }
 
-    // Initializing variables
-    $acc_no = $title = $category = $author = $publisher = $isbn = $copyright_year = $class_no = $status = $comment = "";
-    $copies = $price = 0;
-
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["insert"])) {
         $acc_no = $_POST["acc_no"] ?? "";
         $title = $_POST["title"];
+
+        $book_image = $_FILES["book_image"]["name"];
+        $image_size=$_FILES['book_image']['size'];
+        $image_tmp_name=$_FILES['book_image']['tmp_name'];
+        $image_folder="Assets/uploaded_images/".$book_image;
+        
         $category = $_POST["category"];
         $author = $_POST["author"];
         $copies = $_POST["book_copies"];
@@ -23,12 +25,14 @@
         $price = $_POST["book_price"];
         $status = $_POST["status"];
         $comment = $_POST["comment"] ?? "";
+        
 
         if(!(empty($title) || empty($category) || empty($author) || empty($publisher) || empty($isbn))) {
             // Inserting a Book
-            $add_query = "INSERT INTO books (acc_no, title, category, author, copies, publisher, isbn, copyright_year, class_no, price, status, comment) VALUES (
+            $add_query = "INSERT INTO books (acc_no, title, image, category, author, copies, publisher, isbn, copyright_year, class_no, price, status, comment) VALUES (
                     '$acc_no',
                     '$title',
+                    '$book_image',
                     '$category',
                     '$author',
                     $copies,
@@ -39,18 +43,32 @@
                     $price,
                     '$status',
                     '$comment')";
-
-            if(mysqli_query($conn,$add_query)) {
-                echo 
-                "<script type=\"text/javascript\"> 
-                    window.alert('Book added successfully');
-                    window.location.href = 'addBook.html';
-                </script>";
+            
+            if (mysqli_query($conn, $add_query)) {
+                if ($image_size > 2000000) {
+                    echo 
+                    "<script type=\"text/javascript\"> 
+                        window.alert('Image size is too large');
+                        window.location.href = 'addBook.html';
+                    </script>";
+                } elseif (!move_uploaded_file($image_tmp_name, $image_folder)) {
+                    echo 
+                    "<script type=\"text/javascript\"> 
+                        window.alert('Failed to upload image. Please try again.');
+                        window.location.href = 'addBook.html';
+                    </script>";
+                } else {
+                    echo 
+                    "<script type=\"text/javascript\"> 
+                        window.alert('Book added successfully');
+                        window.location.href = 'addBook.html';
+                    </script>";
+                }
             } else {
                 echo 
                 "<script type=\"text/javascript\"> 
                     window.alert('Sorry! Failed to add book'); 
-                     window.location.href = 'addBook.html';
+                    window.location.href = 'addBook.html';
                 </script>";
             }
         }
