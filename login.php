d58efe174eff86c5cbff3d32a32f54a60cf826a9
NuzrathR <nrauff97@gmail.com>
2024-12-19 14:51:30 +0530

    Update EditProfile.php

--- login.php
+++ login.php
@@ -78,17 +78,8 @@
 </head>
 <body>
     <?php
-        // Starting the session
-        session_start();
-
-        // Setting up the connection
-        $conn = mysqli_connect("localhost", "root", "fazna11aa99zz@Sheriffdeen", "booknest_db");
-
-        // Checking if the connection was successful
-        if (!$conn) {
-            die("Connection Unsuccessful: " . mysqli_connect_error());
-        }
-
+        include 'config.php';
+       session_start();
         // Initializing variables
         $usernameErr = $passwordErr = "";
         $username = $password = "";
