e52171a83a111a94eaf80ea6dfee9aea5a988e57
DESKTOP-L8A9RC2\Fazna <gfsheriffdeen@gmail.com>
2025-03-08 22:21:54 +0530

    Updated chatbot styles

--- chatbot.php
+++ chatbot.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Management System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="chatbot.css">
 </head>
 <body>
@@ -11,11 +12,24 @@
         <div class="chat-box" id="chat-box">
             <div class="chat-header">
                 <h2>Chatbot</h2>
-                <button id="close-chat">X</button>
+                <button id="close-chat">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </button>
             </div>
-            <div class="chat-messages" id="chat-messages"></div>
-            <input type="text" id="user-input" placeholder="Type your message..." />
-            <button id="send-button">Send</button>
+            <div class="chat-message-container">
+                <div class="chat-message">
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                    <div class="bot">
+                        Hello there, how can I help you?
+                    </div>
+                </div>
+            </div>
+            <div class="message-input">
+                <input type="text" id="user-input" placeholder="Type your message..." />
+                <button type="submit" id="send-button">
+                    <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                </button>
+            </div>
         </div>
         <button id="open-chat">Chat with us</button>
     </div>
